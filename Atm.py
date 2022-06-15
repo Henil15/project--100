40bae8de47996967ea35c460fa026184d58dba30
Henil15 <75368846+Henil15@users.noreply.github.com>
2022-06-15 20:53:47 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,13 @@
+class Atm(object):
+    def _init_(self,cardnumber,pinnumber):
+       self.cardnumber=cardnumber
+       self.pinnumber=pinnumber
+       
+
+def CashWithdrawl(self):
+    print("you have withdrawed 800 rupees")
+    
+     
+def BalanceEnquiry(self):
+    print("you have 200 rupees balance")
+
